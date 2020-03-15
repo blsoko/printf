83852fa6b1024b77c61da0774e59160760b9af41
Edison Giraldo A <1470@holbertonschool.com>
2020-03-15 22:34:40 +0000

    changes to holberton print_string print_char _putchar and main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_printf("Printing a simple sentence\n");
+	_printf("Printing Q char: %c\n", 'Q');
+	_printf("Printing char: %c, and printing string: %s\n", 'Q', "Jose and Edison project");
+	return (0);
+}
