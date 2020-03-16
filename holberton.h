2da01a22cf27883555b3179d123218a39a4ba022
blsoko <exanymejose1@hotmail.com>
2020-03-16 03:46:56 +0000

    Incomplete

--- holberton.h
+++ holberton.h
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-	typedef struct conversion
-	{
-		char c;
-		void (*f)(va_list);
-	}S_conversion;
+int _putchar(char c);
+typedef struct conversion
+{
+	char *c;
+	void (*f)(record);
+}s_conversion;
 
 int _printf(const char *format, ...);
-void print_string(va_list);
-void print_char(va_list);
+void print_string(record);
+void print_char(record);
 
 #endif /*HOLBERTON_H*/
