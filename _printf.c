0b360d430a1910dbdca00ac2dce5815955cb7c7b
blsoko <exanymejose1@hotmail.com>
2020-03-16 05:25:11 +0000

    backslash conditions

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
         int i = 0;
         va_list record; /* type data to use in our structure*/
 
-        s_convertion farray[] = {
+        S_convertion farray[] = {
                 {"s", print_string}
                 {"c", print_character}
                 {"d", print_decimal}
@@ -29,7 +29,7 @@ int _printf(const char *format, ...)
                         if(format[j] != '\\' )
                         {
                                 
-                                if (farray[i].c[0] == format[j + 1] && format[j] == %) /* verify %option */
+                                if ((farray[i].c[0] == (format[j + 1] != '\0')) && format[j] == %) /* verify %option */
                                 {
                                         (farray[i].f)(record);
                                 }
@@ -41,10 +41,44 @@ int _printf(const char *format, ...)
                         else
                         {
                                 valuespecial(format[j + 1]);
+                                j++;
                         }
                         i++;
                 }
                 j++;
                 i = 0;
+        }
+}
+
+/**
+ * valuespecial - check special character '\'
+ *
+ * @caracter: format[j + 1] value 
+ *
+ * Return: A value
+ */
+void valuespecial(char caracter)
+{
+        char narray[] = {'\\', '?', '"', '0'}
+        int i = 0;
+        if (caracter == 'n')
+        {
+                _putchar('\n');
+        }
+        else if (carater == 't')
+        {
+                while(i < 4)
+                {
+                        _putchar(" ");
+                        i++;
+                }
+        }
+
+        for (i = 0; narray[i] != '0'; i++)
+        {
+                if (narray[i] == caracter)
+                {
+                        _putchar(narray[i]);
+                }
         }
 }
