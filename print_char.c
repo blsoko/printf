09b767225795dd1ef7de677c1f9f5eaf495fc6d0
Edison Giraldo A <1470@holbertonschool.com>
2020-03-17 16:07:36 +0000

    Exc 0

--- print_char.c
+++ print_char.c
@@ -3,10 +3,12 @@
 /**
  * print_char - function that prints a char.
  *
+ * @bytes_printed: bytes printed.
  * @list: list of parameters with char to print.
  */
 
-void print_char(va_list list)
+void print_char(int *bytes_printed, va_list list)
 {
+	(*bytes_printed)++;
 	_putchar(va_arg(list, int));/*get the char and print it*/
 }
