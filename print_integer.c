ad6e2f00e56ce312e79bf25efc4f4fac2846edbc
blsoko <exanymejose1@hotmail.com>
2020-03-17 17:21:23 +0000

    issue with betty, file integers

--- print_integer.c
+++ print_integer.c
@@ -1,40 +1,53 @@
 #include "holberton.h"
 
 /**
- * print_integer - function that prints a char.
+ * print_number - function that prints a char.
  *
- * @list: list of parameters with char to print.
+ * @n: list of parameters with char to print.
  */
 
 void print_number(int n)
 {
 	unsigned int m = 0;
-        if (n < 0)
+
+	if (n < 0)
 	{
 		m = -n;
 		_putchar('-');
 	}
-        else
-        {
-                m = n;
-        }
+	else
+	{
+		m = n;
+	}
 	while (m / 10)
 	{
 		print_number(m / 10);
 		break;
 	}
 	_putchar((m % 10) + '0');
-}
+	}
 
-
+/**
+ * print_integer - calls a function
+ *
+ * @list: value
+ */
 void print_integer(va_list list)
 {
 	int n = va_arg(list, int);
-        print_number(n);
+
+	print_number(n);
 }
+
+/**
+ * print_integerofi - call a function to print
+ *
+ * @list: value valist
+ */
 
 void print_integerofi(va_list list)
 {
 	int n = va_arg(list, int);
-        print_number(n);
+
+	print_number(n);
 }
